57696f9c70e231b6a7505b7184b615487eab6eac
Mofumimi <112867280+Mofumimi@users.noreply.github.com>
2023-11-09 09:27:30 +0100

    update

--- Órai/2023-11-09/ayo2.c
+++ Órai/2023-11-09/ayo2.c
@@ -1,9 +1,10 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main(){
-srand(time(0));
+srand(time(NULL));
 int random_number = rand() % 100 + 1;
 
 printf("%d", random_number);
